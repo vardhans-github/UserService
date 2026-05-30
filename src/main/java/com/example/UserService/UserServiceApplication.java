d7db226b5c780d44a5fab18bd8bebce00cedca71
vardhans-github <yashvardhan.malve00@gmail.com>
2026-05-31 01:19:44 +0530

    Fixed dependecies, POM.xml

--- src/main/java/com/example/UserService/UserServiceApplication.java
+++ src/main/java/com/example/UserService/UserServiceApplication.java
@@ -1,8 +1,10 @@
 package com.example.UserService;
-
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UserServiceApplication {
 
